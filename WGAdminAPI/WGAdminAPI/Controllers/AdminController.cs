d0f270f9b9ab955682f9d7d3cbee97a70d8a87aa
Tanner Trimble <mrtannertrimble@gmail.com>
2022-02-07 21:20:48 -0700

    removed comments

--- WGAdminAPI/WGAdminAPI/Controllers/AdminController.cs
+++ WGAdminAPI/WGAdminAPI/Controllers/AdminController.cs
@@ -18,8 +18,6 @@ namespace WGAdminAPI.Controllers
             return output;
         }
 
-<<<<<<< HEAD
-=======
         [HttpGet("reset")]
         public Task<string> ResetAdmin()
         {
@@ -29,6 +27,6 @@ namespace WGAdminAPI.Controllers
 
             return output;
         }
->>>>>>> 4a259bce62d7aa6ca93f2ebfd8b63978b611d325
+
     }
 }
